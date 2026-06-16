80ebd939db42924309d9134d3da948eca5f7eab8
Purnima Singh <purnimaasingh43@gmail.com>
2026-06-17 00:26:14 +0530

    modify edit url

--- src/app/admin/blog/edit/page.jsx
+++ src/app/admin/blog/edit/page.jsx
@@ -97,7 +97,7 @@ export default function EditBlog() {
 
     try {
       const token = localStorage.getItem("token") || "";
-      const response = await fetch(`http://localhost:5000/api/blogs/${id}`, {
+      const response = await fetch(`https://ca-service.onrender.com/api/blogs/${id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
